9339dbb7de9af2565227618d5dd78b1007de0cf6
AdrianP2424 <guiacuencaapp@gmail.com>
2026-04-15 15:57:14 -0500

    Add Estudiantes Llist

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Práctica: 04.01 Complejidad Proyecto JAVA
+
+## Datos del Estudiante
+- **Nombre:** Jose Adrian Plaza Salto
+- **Curso:** Estructura de Datos G2
+- **Fecha:** 14/04/2026
+
+---
+
+## 1. [Título de la sección] o [Practica]
+**Fecha:** 15/04/2026
+
+**Descripción:** Creamos el proyecto y lo subimos a Github
+
+---
+
+## 2. icc-est-u4-complejidad
+![Descripción de la captura](assets/image.png)
+**Fecha:** 15/04/26
+**Descripción:** Creamos la clase Estuantes y Generadores y creamos un listado de estudiantes con datos aleatorios para buscar y optimizar la busqueda
+
+---
---
+++ src/App.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int cantidadEstudiantes = 30000000;  
+        //int cantidad = 30_000_000;
+        List<Estudiante> estudiantes = Generador.generarListado(cantidadEstudiantes);     
+        
+        //for (Estudiante est : estudiantes){
+        //    System.out.println(est.nombre);
+        //}
+
+        String nombreBuscar = "Estudiante_2900111";
+        int intentos = 0;
+        for (Estudiante est : estudiantes){
+            if(est.nombre.equals(nombreBuscar)){
+                System.out.println("EXISTE");
+                break;
+            }else {
+                intentos++;    
+            }
+        }
+        System.out.println(intentos);
+        System.out.println("FIN"); 
+    }
+
+}
